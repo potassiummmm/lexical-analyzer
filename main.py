864c042f545d477c01b8367f25e0c180be9f21a9
potassiummmm <1574996073@qq.com>
2021-12-27 16:36:19 +0800

    Update test cases of dfa minimization

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-from finite_automata.dfa import DFA
+from finite_automata import DFA
 
 if __name__ == '__main__':
-    dfa = DFA(
-        all_states={'0', '1', '2', '3', '4', '5', '6'},
-        input_alphabet={'a', 'b'},
-        transitions={
-            '0': {'a': '1', 'b': '2'},
-            '1': {'a': '3', 'b': '2'},
-            '2': {'a': '1', 'b': '5'},
-            '3': {'a': '3', 'b': '4'},
-            '4': {'a': '6', 'b': '5'},
-            '5': {'a': '6', 'b': '5'},
-            '6': {'a': '3', 'b': '4'},
-        },
-        initial_state='0',
-        final_states={'3', '4', '5', '6'}
-    )
-    dfa.show_diagram(path='output/DFA/before_minimization.png')
-    new_dfa = dfa.minimize()
-    new_dfa.show_diagram(path='output/DFA/after_minimization.png')
-
+    pass
